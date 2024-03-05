b8ee206fd5765cb66e6f3f4710fb600de508587e
Caio Diamantino <caiodiamantino53@gmail.com>
2024-03-05 11:15:49 -0300

    feat(ap07): fim da sessaão 2.4

--- script.sql
+++ script.sql
@@ -1,11 +1,11 @@
-CREATE TABLE tb_usuario{
+CREATE TABLE tb_usuario(
 	cod_usuario SERIAL PRIMARY KEY,
 	login VARCHAR(200) NOT NULL,
 	senha VARCHAR(200) NOT NULL
-}
+);
 
 INSERT INTO tb_usuario(login, senha) VALUES
 ('admin' , 'admin'),
-('Joao' , '123456')
+('Joao' , '123456');
 
-SELECT * FROM tb_usuario
+SELECT * FROM tb_usuario;
